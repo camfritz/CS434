fdcb31fdf01726f0b0fc0671e58410e0e958135b
Camron Fritz <camfritz@gmail.com>
2019-04-08 17:12:11 -0700

    Add line, square drawing solutions.

---
+++ hw1/Escape/escape.py
@@ -0,0 +1,63 @@
+import turtle
+
+def draw_bag():
+	turtle.shape('turtle')
+	turtle.pen(pencolor='brown', pensize=5)
+	turtle.penup()
+	turtle.goto(-35, 35)
+	turtle.pendown()
+	turtle.right(90)
+	turtle.forward(70)
+	turtle.left(90)
+	turtle.forward(70)
+	turtle.left(90)
+	turtle.forward(70)
+
+def escaped(position):
+	x = int(position[0])
+	y = int(position[1])
+	return x < -35 or x > 35 or y < -35 or y > 35
+
+def draw_line():
+	angle = 0
+	step = 5
+	t = turtle.Turtle()
+	t.shape('turtle')
+	t.pen(pencolor='red', pensize=3)
+	while not escaped(t.position()):
+		t.left(angle)
+		t.forward(step)
+
+def draw_square(t, size):
+	L = []
+	for i in range(4):
+		t.forward(size)
+		t.left(90)
+		store_position_data(L, t)
+	return L
+
+def store_position_data(L, t):
+	position = t.position()
+	L.append([position[0], position[1], escaped(position)])
+
+def draw_squares(number):
+	t = turtle.Turtle()
+	L = []
+	for i in range(1, number + 1):
+		t.penup()
+		t.goto(-i, -i)
+		t.pendown()
+		L.extend(draw_square(t, i * 2))
+	return L
+
+def draw_squares_until_escaped(n):
+	t = turtle.Turtle()
+	L = draw_squares(n)
+	with open("data_square", "wb") as f:
+		pickle.dump(L, f)
+
+if(__name__) == '__main__':
+	turtle.setworldcoordinates(-70.0, -70.0, 70.0, 70.0)
+	draw_bag()
+	draw_squares_until_escaped(100)
+	turtle.mainloop()
